018b1d3120f780a62f2aa2eadbc96cf9aaad7882
gabriel-a-caldas <gabrieldeacaldas@gmail.com>
2023-08-09 16:13:05 -0300

    Revert "Commenting the code"
    This reverts commit 1252d8af6b2e9bfe0ebaf9aebfc8af2fd9e4c00c.

--- swarm-app-1/assignment.sh
+++ swarm-app-1/assignment.sh
@@ -4,17 +4,13 @@ docker network create --driver overlay frontend
 # Create the backend network
 docker network create --driver overlay backend
 
-# Create the voting app
+# Create the voting app create 
 docker service create --name voting-app --replicas 2 --publish published=80,target=80 --network frontend bretfisher/examplevotingapp_vote
 
-# Create the redis application
 docker service create --name redis --replicas 1 --network frontend redis:3.2
 
-# Create the worker application
 docker service create --name worker --replicas 1 --network frontend --network backend bretfisher/examplevotingapp_worker
 
-# Create the db application
 docker service create --name db --replicas 1 --network backend -e POSTGRES_HOST_AUTH_METHOD=trust --mount type=volume,source=db-data,destination=/var/lib/postgres/data
 
-# Create the result app which listens on port 5001
 docker service create --name result --replicas 1 --publish published=5001,target=80 --network backend bretfisher/examplevotingapp_result
